dd6747ef23ac98c0c881affe903dba52a9b3f8be
Thanh Huynh <hu.thanh89@yahoo.com>
2018-12-03 21:36:56 -0800

    formatting

--- view/common/Target.js
+++ view/common/Target.js
@@ -3,7 +3,7 @@
 //----------------------------------------------------------------------------//
 
 Ext.define('view.common.Target', {
-    extend: 'Ext.dd.DDTarget',
+    extend:    'Ext.dd.DDTarget',
     ignoreSelf: false
 });
 
